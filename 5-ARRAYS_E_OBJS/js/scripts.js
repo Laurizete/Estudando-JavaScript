328f6a768ed3c33e1259476e1293ed29d0499b7e
Laurizete <lorealves2000@gmail.com>
2026-04-13 17:42:30 -0300

    atualizando

--- 5-ARRAYS_E_OBJS/js/scripts.js
+++ 5-ARRAYS_E_OBJS/js/scripts.js
@@ -33,7 +33,7 @@ console.log(myName.length); // - length é uma propriedade das strings que retor
 
 console.log(myName["length"]); // - length é uma propriedade das strings que retorna o número de caracteres presentes na string
 
-// metodos dos arrays
+//4 metodos dos arrays
 
 const numeros2 = [1, 2, 3, 4, 5];
 
@@ -68,7 +68,7 @@ console.log(texto.toLowerCase()); // - toLowerCase é um método das strings que
 
 console.log(texto.indexOf("g ")); // - indexOf é um método das strings que retorna o índice da primeira ocorrência de uma substring especificada
 
-// 4 - objetos
+// 5 - objetos
 
 const pessoa = {
     nome: "João",
@@ -100,7 +100,7 @@ console.log(pessoa.nome.length); // - acessando a propriedade nome do objeto pes
 
 console.log(pessoa["nome"].length); // - acessando a propriedade nome do objeto pessoa usando a notação de colchetes e depois acessando a propriedade length da string nome, retorna o número de caracteres da string nome
 
-// 5 - mais sobre objetos criando e deletando propriedades
+// 6 - mais sobre objetos criando e deletando propriedades
 
 const carro = {
     marca: "Toyota",
@@ -131,4 +131,76 @@ delete carro["marca"]; // - deletando a propriedade marca do objeto carro usando
 
 console.log(carro);
 
+// 7 - mais sobre objetos 
+const pessoa2 = {
+    nome: "Maria",
+    idade: 25,
+    cidade: "Rio de Janeiro",
+    saudacao: function() { // - adicionando um método saudacao ao objeto pessoa2
+        return `Olá, meu nome é ${this.nome} e eu tenho ${this.idade} anos.`; // - usando a palavra-chave this para acessar as propriedades do objeto dentro do método
+    }
+};
 
+console.log(pessoa2);
+
+console.log(pessoa2.saudacao()); // - chamando o método saudacao do objeto pessoa2  
+
+const obj = {
+    prop1: "valor1",
+    prop2: "valor2",
+};
+
+console.log(obj) instanceof Object; // - verificando se obj é uma instância de Object, retorna true
+
+
+ const obj2 = {
+    prop3: [],
+ }
+Object.assign(obj2, obj); // - usando Object.assign para copiar as propriedades de obj para a propriedade prop3 do objeto obj2
+
+console.log(obj2);
+
+// 8 - conhecendo melhor os objetos
+console.log(Object.keys(obj)); // - Object.keys é um método dos objetos que retorna um array com as chaves do objeto
+
+console.log(Object.values(obj)); // - Object.values é um método dos objetos que retorna um array com os valores do objeto
+
+console.log(Object.entries(obj)); // - Object.entries é um método dos objetos que retorna um array com os pares chave-valor do objeto
+
+//  - objetos e arrays
+
+const pessoa3 = {
+    nome: "Carlos",
+    idade: 40,
+    cidade: "Belo Horizonte",
+    hobbies: ["futebol", "música", "viagens"] // - adicionando uma propriedade hobbies que é um array
+};
+
+console.log(pessoa3);
+
+console.log(pessoa3.hobbies); // - acessando a propriedade hobbies do objeto pessoa3, retorna o array de hobbies
+
+console.log(pessoa3.hobbies[0]); // - acessando o primeiro elemento do array de hobbies do objeto pessoa3, retorna "futebol"
+
+console.log(pessoa3.hobbies[1]); // - acessando o segundo elemento do array de hobbies do objeto pessoa3, retorna "música"
+
+console.log(pessoa3.hobbies[2]); // - acessando o terceiro elemento do array de hobbies do objeto pessoa3, retorna "viagens"
+
+// 9 - mutação 
+
+const a = {
+    nome: "Lore",
+};
+const b = a; 
+console.log(a); // - b é uma referência ao mesmo objeto que a, então ambos apontam para o mesmo objeto na memória
+console.log(b); // - a é uma referência ao objeto pessoa4, então ambos apontam para o mesmo objeto na memória
+
+console.log(a === b); // - a e b são referências ao mesmo objeto, então a comparação retorna true
+
+a.age = 25; // - modificando a propriedade age do objeto pessoa4 através da referência a
+ console.log(a); // - a agora tem a propriedade age, pois a e b apontam para o mesmo objeto
+ console.log(b); // - b também tem a propriedade age, pois a e b apontam para o mesmo objeto
+
+ delete b .age; // - deletando a propriedade age do objeto pessoa4 através da referência b
+ console.log(a); // - a não tem mais a propriedade age, pois a e b apontam para o mesmo objeto
+ console.log(b); // - b não tem mais a propriedade age, pois a e b apontam para o mesmo objeto
