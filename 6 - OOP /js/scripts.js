6a944f03a17d1bdb997478661a59932d2825f365
Laurizete <lorealves2000@gmail.com>
2026-04-28 16:28:55 -0300

    atualizando

--- 6 - OOP /js/scripts.js
+++ 6 - OOP /js/scripts.js
@@ -27,3 +27,27 @@ pessoa.setNome('Maria');
 console.log(pessoa.getNome());
 
 // 3 - prototypes
+ const texto = "Olá, mundo!";
+
+ console.log (Object.getPrototypeOf(texto));
+ const booleano = true;
+ console.log(Object.getPrototypeOf(booleano));
+ const arr = [];
+ console .log(arr.length);
+ console.log(Object.getPrototypeOf(arr));
+
+ // 4 - mais sobre prototypes
+ function pessoa(nome) {
+    this.nome = nome;
+ }
+ pessoa.prototype.getNome = function() {
+    return this.nome;
+ };
+ const pessoa1 = new pessoa("Alice");
+ console.log(pessoa1.getNome());
+ const pessoa2 = new pessoa("Bob");
+ console.log(pessoa2.getNome());
+   
+ 
+
+
